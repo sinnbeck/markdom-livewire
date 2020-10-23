dc1b6a20f115b2ca95d9a7fbbc1a4fa756e96b09
René Sinnbeck <rene.sinnbeck@gmail.com>
2020-10-23 14:15:24 +0200

    Remove test data

--- app/Http/Livewire/MarkdownPage.php
+++ app/Http/Livewire/MarkdownPage.php
@@ -13,43 +13,4 @@ class MarkdownPage extends Component
         return view('livewire.markdown-page');
     }
 
-    public function index()
-    {
-        $markdown =
-<<<markdown
-# Title
-## Subtitle
-
-* List item
-* List item 2
-1. Numbered list item
-2. Numbered list item 2
-
-### Code
-Php
-```
-$this->foo = 'bar';
-```
-Css
-```
-.title  {
-    text-transform: uppercase;
-    font-weight: bold;
-    font-size: 1.3rem;
-}
-```
-Inline js code `const $x = [1, 2, 3];`
-
-### Text formatting
-**Bold** \
-__Bold__ \
-_Italic_ \
-~~Crossed~~
-
-### Escaped html
-<script>alert()</script>
-markdown;
-
-        return view('markdown.index', compact('markdown'));
-    }
 }
